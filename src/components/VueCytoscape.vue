2c7c1c5dc4ebb9ef82660d6d6fdc22d1c398ae47
lana <lanakona.lb@gmail.com>
2022-12-20 16:45:50 +0200

    visualise activities

--- src/components/VueCytoscape.vue
+++ src/components/VueCytoscape.vue
@@ -28,13 +28,71 @@ import cytoscape from 'cytoscape';
         methods: {
           displayDiagram(){
             var cy = cytoscape({
-                container: document.getElementById('cy')
+                container: document.getElementById('cy'),
+                autoungrabify: true,
+                layout: {
+                  name: 'grid'
+                },
+
+                style: [
+                {
+                  selector: 'node',
+                  css: {
+                    'label': 'data(id)',
+                    'text-valign': 'center',
+                    'text-halign': 'center',
+                    'shape': 'round-rectangle',
+                    'background-color' : 'white',
+                    'border-width' : 1,
+                    'border-color' : 'black',
+                    'padding' : 2,
+                    'text-wrap' : 'wrap',
+                    'text-max-width' : 10,
+                    'height' : 80,
+                    'width' : 100,
+                    'font-size' : 12,
+                    'font-family' : 'arial'
+                  }
+                },
+                {
+                  selector: 'edge',
+                  css: {
+                    'curve-style': 'taxi',
+                    'target-arrow-shape': 'triangle',
+                    'line-color' : 'black',
+                    'target-arrow-color': 'black',
+                    'width': 1
+                  }
+                }
+              ],
             });
-            cy.add([
-                { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
-                { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
-                { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
-                ]);
+            
+            var elems = [];
+            for (var i = 0; i < this.activities.length; i++) {
+              elems.push({
+                group: "nodes",
+                data: {
+                  id: this.activities[i].name, 
+                },
+                position:{
+                  x: (100 + 150*i),
+                  y: 100
+                }
+              });
+              if(i == this.activities.length - 1) break;
+
+              elems.push({
+                group: "edges",
+                data: {
+                  id: "e" + i,
+                  source: this.activities[i].name,
+                  target: this.activities[i+1].name,
+                  label: "e" + i
+                }
+              });
+            }
+
+            cy.add(elems)
 
                             
             },
