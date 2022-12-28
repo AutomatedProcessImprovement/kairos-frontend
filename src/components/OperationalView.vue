f5dff25321b1807e3c5ce24d83bcd0fcb4e41e3a
lana <lanakona.lb@gmail.com>
2022-12-28 15:42:49 +0200

    VIS-68 display prediction in operational view

--- src/components/OperationalView.vue
+++ src/components/OperationalView.vue
@@ -1,28 +1,29 @@
 <template>    
     <div class="operational-view">
       <div class="column">
-        <div class="prediction-container">
+        <div class="prediction-container" v-if="currentCase.prediction !== undefined">
             <h4>Prediction</h4>
-            <small>{{currentCase.prediction}}</small>
-            <!-- <small>Case predicted to last {{ currentCase.prediction.effect }} {{ kpi.measurement }}.</small> -->
-            <!-- <small v-show="currentCase.prediction.effect > kpi.value">This violates the target.</small> -->
-            <!-- <br/> -->
-            <!-- <small>Probability: {{currentCase.prediction.probability}}% Uncertainty: {{currentCase.prediction.uncertainty}}%</small> -->
+            <div class="prediction-details">
+              <small>Case predicted to last {{ currentCase.prediction.effect }} {{ kpi.measurement }}.</small>
+              <small v-if="currentCase.prediction.effect > kpi.value"> This violates the target.</small>
+              <br/>
+              <small>Probability: {{currentCase.prediction.probability}}% Uncertainty: {{currentCase.prediction.uncertainty}}%</small>
+            </div>
         </div>
         <div class="recommendations-list">
           <h4>Recommendations list</h4>
           <div class="recommendation" v-for="(r,index) in currentCase.recommendations" :key="index">
             <div class="recommendation-heading">
-                <p>{{r.name}}</p>
-                <button class="btn" @click="selectRecommendation(index)" :class="{selected: index === selectedRec}">See details</button>
+              <p>{{r.name}}</p>
+              <button class="btn" @click="selectRecommendation(index)" :class="{selected: index === selectedRec}">See details</button>
             </div>
             <small>Predicted effect: case lasts {{r.effect}} {{ kpi.measurement }}</small>
             <div class="similar-cases">
-                <accordion-component>
-                  <template v-slot:title>
-                    <span>Similar cases</span>
-                  </template>
-                  <template v-slot:content>
+              <accordion-component>
+                <template v-slot:title>
+                  <span>Similar cases</span>
+                </template>
+                <template v-slot:content>
                     <div class="similar-cases-table">
                       <table :class="{expanded: index === selectedSimilarCases}">
                           <thead>
