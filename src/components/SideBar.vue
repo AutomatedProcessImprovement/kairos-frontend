0abbfc24313575abc1c0e8300cbdffb1b777508f
lana <lanakona.lb@gmail.com>
2023-01-04 11:48:05 +0200

    VIS-69 fix sidebar-border height

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <aside class="sidebar">
+  <div class="sidebar">
         <div class="sidebar-items">
           <router-link class = "sidebar-item"  :to="{ name: 'home'}"><ion-icon name="home-outline"></ion-icon>  Home</router-link>
           <router-link class = "sidebar-item"  :to="{ name: 'cases'}"><ion-icon name="stats-chart-outline"></ion-icon>  Cases</router-link>
           <router-link class = "sidebar-item"  :to="{ name: 'resources'}"><ion-icon name="people-outline"></ion-icon>  Resources</router-link>
         </div>
-  </aside>
+    </div>
 </template>
 
 <script>
